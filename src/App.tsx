4fa70bd80375af9dbd36025e5ffa80bb9d5c123d
Tanner <thetannerthomas@gmail.com>
2022-01-30 17:50:21 -0700

    add comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { SelectCategory } from "./features/quiz";
 
+/**
+ * @component
+ *
+ * @description
+ *  Main entry point for the application.
+ *  Handles routing for the application.
+ *
+ * @returns {JSX.Element}
+ */
+
 function App() {
   return (
     <BrowserRouter>
