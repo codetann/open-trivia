a405a9b5697881a244f533ceefb6d1de2f194e98
Tanner <thetannerthomas@gmail.com>
2022-01-30 17:51:02 -0700

    update icons for categories

---
+++ src/features/quiz/data/categories.ts
@@ -0,0 +1,124 @@
+import * as icons from "react-icons/fa";
+
+export const categories = [
+  {
+    title: "General Knowledge",
+    icon: icons.FaBrain,
+    code: 9,
+  },
+  {
+    title: "Books",
+    icon: icons.FaBook,
+    code: 10,
+  },
+  {
+    title: "Film",
+    icon: icons.FaFilm,
+    code: 11,
+  },
+  {
+    title: "Music",
+    icon: icons.FaMusic,
+    code: 12,
+  },
+  {
+    title: "Musicals & Theatres",
+    icon: icons.FaTheaterMasks,
+    code: 13,
+  },
+  {
+    title: "Television",
+    icon: icons.FaTv,
+    code: 14,
+  },
+  {
+    title: "Video Games",
+    icon: icons.FaGamepad,
+    code: 15,
+  },
+  {
+    title: "Board Games",
+    icon: icons.FaDice,
+    code: 16,
+  },
+  {
+    title: "Science & Nature",
+    icon: icons.FaMicroscope,
+    code: 17,
+  },
+  {
+    title: "Computers",
+    icon: icons.FaLaptopCode,
+    code: 18,
+  },
+  {
+    title: "Mathematics",
+    icon: icons.FaCalculator,
+    code: 19,
+  },
+  {
+    title: "Mythology",
+    icon: icons.FaPuzzlePiece,
+    code: 20,
+  },
+  {
+    title: "Sports",
+    icon: icons.FaFutbol,
+    code: 21,
+  },
+  {
+    title: "Geography",
+    icon: icons.FaMapMarkerAlt,
+    code: 22,
+  },
+  {
+    title: "History",
+    icon: icons.FaBone,
+    code: 23,
+  },
+  {
+    title: "Politics",
+    icon: icons.FaGavel,
+    code: 24,
+  },
+  {
+    title: "Art",
+    icon: icons.FaArtstation,
+    code: 25,
+  },
+  {
+    title: "Celebrities",
+    icon: icons.FaUserAlt,
+    code: 26,
+  },
+  {
+    title: "Animals",
+    icon: icons.FaPaw,
+    code: 27,
+  },
+  {
+    title: "Vehicles",
+    icon: icons.FaCarAlt,
+    code: 28,
+  },
+  {
+    title: "Comics",
+    icon: icons.FaPenFancy,
+    code: 29,
+  },
+  {
+    title: "Gadgets",
+    icon: icons.FaLaptopCode,
+    code: 30,
+  },
+  {
+    title: "Anime & Manga",
+    icon: icons.FaDragon,
+    code: 31,
+  },
+  {
+    title: "Cartoon & Animations",
+    icon: icons.FaGhost,
+    code: 32,
+  },
+];
